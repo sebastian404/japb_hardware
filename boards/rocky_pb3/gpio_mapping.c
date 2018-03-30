f28458d384819d71d3ec082e9ea83557415d2a42
Sebastian Robinson <sebastian@podtwo.com>
2018-03-30 23:59:19 +0100

    add Rocky GPIO mappings

---
+++ boards/rocky_pb3/gpio_mapping.c
@@ -0,0 +1,7 @@
+// Video Detect                   0=CGA / 1=VGA
+static const int mk_videomode = { -1            };
+
+// GPIO mapping                            service, coin, start, up, down, left, right, a,  b,  c,  x,  y,  z
+static const int mk_arcade_gpio1_maps[] = {C8,      -1,   B2,    B14,B15,  B16,  E4,    E5, E6, E7, E8, E9, E10 };
+static const int mk_arcade_gpio2_maps[] = {-1 ,     -1,   C9,    C10,C11,  C12,  C13,   C14,C15,C19,C18,C17,C16 }; 
+
