c82f96ac11594f6bf51a966af2d49ae61fc72496
Sebastian Robinson <sebastian@podtwo.com>
2018-03-31 00:07:06 +0100

    add hb Pandora's Key Boards

---
+++ boards/hb_pk5s/gpio_mapping.c
@@ -0,0 +1,7 @@
+// Video Detect                   0=CGA / 1=VGA
+static const int mk_videomode = { -1            };
+
+// GPIO mapping                            service, coin, start, up, down, left, right, a,  b,  c,  x,  y,  z
+static const int mk_arcade_gpio1_maps[] = {B2,      B0,   C15,   C14, C13, C12,  C11,   E0, E1, E2, E3, E4, E5 }
+static const int mk_arcade_gpio2_maps[] = {-1 ,     B4,   C10,   C9,  C8,  C7,   C6,    C0, C1, C2, C3, C4, C5 }
+
